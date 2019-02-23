d40d31fb7f4cb2d64decdfd02caddc1495622241
Christian Gustavo <christiangomes00@gmail.com>
2019-02-23 14:53:53 -0300

    LoginComponent: interface com Angular Material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   exports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
