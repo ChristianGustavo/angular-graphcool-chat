eaea087cfe662e9d9e9cfafd816aa85197b36827
Christian Gustavo <christiangomes00@gmail.com>
2019-03-09 19:54:56 -0300

    UserService: método para buscar usuário por id

--- src/app/core/services/user.graphql.ts
+++ src/app/core/services/user.graphql.ts
@@ -1,8 +1,13 @@
 import gql from 'graphql-tag';
+
 import { User } from '../models/user.model';
 
 export interface AllUsersQuery {
   allUsers: User[];
+}
+
+export interface UserQuery {
+  User: User;
 }
 
 export const ALL_USERS_QUERY = gql`
@@ -13,6 +18,17 @@ export const ALL_USERS_QUERY = gql`
         id_not: $idToExclude
       }
     ) {
+      id
+      name
+      email
+      createdAt
+    }
+  }
+`;
+
+export const GET_USER_BY_ID_QUERY = gql`
+  query GetUserByIdQuery($userId: ID!) {
+    User(id: $userId) {
       id
       name
       email
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { User } from './../models/user.model';
-import { ALL_USERS_QUERY, AllUsersQuery } from './user.graphql';
+import { ALL_USERS_QUERY, AllUsersQuery, GET_USER_BY_ID_QUERY, UserQuery } from './user.graphql';
 
 @Injectable({
   providedIn: 'root'
@@ -26,4 +26,14 @@ export class UserService {
         map(res => res.data.allUsers)
       );
   }
+
+  getUserById(id: string): Observable<User> {
+    return this.apollo.query<UserQuery>({
+      query: GET_USER_BY_ID_QUERY,
+      variables: { idUser: id }
+    }).pipe(
+      map(res => res.data.User)
+    );
+  }
+
 }
