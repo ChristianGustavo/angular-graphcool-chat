c0c286d20c6dad8fac1ceb4e1f3a7a4e4a0b0d1d
Christian Gustavo Ribeiro Gomes <christiangomes00@gmail.com>
2019-03-12 17:40:56 -0300

    Apollo Cache Persist e dica sobre recursos Offline com Apollo Client

--- src/app/apollo-config.module.ts
+++ src/app/apollo-config.module.ts
@@ -6,60 +6,70 @@ import { ApolloLink } from 'apollo-link';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 
 import { environment } from './../environments/environment';
 import { GRAPHCOOL_CONFIG, GraphcoolConfig } from './core/providers/graphcool-config.provider';
 import { StorageKeys } from './storage-keys';
 
 @NgModule({
-    imports: [
-        HttpClientModule,
-        ApolloModule,
-        HttpLinkModule
-    ]
+  imports: [
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ]
 })
 export class ApolloConfigModule {
 
-    constructor(
-        private apollo: Apollo,
-        @Inject(GRAPHCOOL_CONFIG) private graphcoolConfig: GraphcoolConfig,
-        private httpLink: HttpLink
-    ) {
+  constructor(
+    private apollo: Apollo,
+    @Inject(GRAPHCOOL_CONFIG) private graphcoolConfig: GraphcoolConfig,
+    private httpLink: HttpLink
+  ) {
 
-        const uri = graphcoolConfig.simpleAPI;
-        const http = httpLink.create({ uri });
+    const uri = graphcoolConfig.simpleAPI;
+    const http = httpLink.create({ uri });
 
-        const authMiddleware: ApolloLink = new ApolloLink((operation, forward) => {
-          operation.setContext({
-            headers: new HttpHeaders({
-              'Authorization': `Bearer ${this.getAuthToken()}`
-            })
-          });
-          return forward(operation);
-        });
+    const authMiddleware: ApolloLink = new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: new HttpHeaders({
+          'Authorization': `Bearer ${this.getAuthToken()}`
+        })
+      });
+      return forward(operation);
+    });
 
-        const linkError = onError(({ graphQLErrors, networkError }) => {
-            if (graphQLErrors) {
-                graphQLErrors.map(({ message, locations, path }) =>
-                    console.log(
-                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-                    ),
-                );
-            }
+    const linkError = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      }
 
-            if (networkError) { console.log(`[Network error]: ${networkError}`); }
-        });
+      if (networkError) { console.log(`[Network error]: ${networkError}`); }
+    });
 
-        apollo.create({
-            link: ApolloLink.from([
-                linkError,
-                authMiddleware.concat(http)
-            ]),
-            cache: new InMemoryCache(),
-            connectToDevTools: !environment.production
-        });
+    const cache = new InMemoryCache();
 
-    }
+    persistCache({
+      cache,
+      storage: window.localStorage
+    }).catch(err => {
+      console.log('Error while persisting cache: ', err);
+    });
+
+    apollo.create({
+      link: ApolloLink.from([
+        linkError,
+        authMiddleware.concat(http)
+      ]),
+      cache,
+      connectToDevTools: !environment.production
+    });
+
+  }
 
   private getAuthToken(): string {
     return window.localStorage.getItem(StorageKeys.AUTH_TOKEN);
