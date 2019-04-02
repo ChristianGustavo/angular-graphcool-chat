416a8f31ed2a23f8e2067f706077cf2f6d861303
Christian Gustavo Ribeiro Gomes <christiangomes00@gmail.com>
2019-04-02 19:27:26 -0300

    Carregando UserModule com Lazy Loading

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { SharedModule } from './../shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [UserProfileComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     UserRoutingModule
-  ]
+  ],
+  declarations: [UserProfileComponent]
 })
 export class UserModule { }
