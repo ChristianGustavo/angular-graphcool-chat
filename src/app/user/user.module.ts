87e0ffed2dcdc1a79aeb21c76c8618fc9b9f13e6
Christian Gustavo Ribeiro Gomes <christiangomes00@gmail.com>
2019-04-02 19:21:43 -0300

    UserProfileComponent para exibir perfil do usuário

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserProfileComponent],
   imports: [
     CommonModule,
     UserRoutingModule
